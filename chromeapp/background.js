9841f05e272fe4ab7d0ba15c2666e236a8da8893
Abdullah Tellioğlu <abdullahtellioglu@tell10glu-MacBook-Air.local>
2017-06-10 05:39:19 +0300

    OK

--- chromeapp/background.js
+++ chromeapp/background.js
@@ -9,35 +9,28 @@ chrome.tabs.onUpdated.addListener(
           "link" :tab.url,
         },
         success: function(result){
-            if(result.type == "Human"){
-                chrome.browserAction.setIcon({path:"icongreen.png"});
-            }else if(result.type== "Bot"){
-                chrome.browserAction.setIcon({path:"iconred.png"});
-            }else{
-                chrome.browserAction.setIcon({path:"icon.png"});
-            }
-        }
-      });
-      $.ajax({
-        method :"GET",
-        dataType:"json",
-        url: "http://127.0.0.1:3547/user",
-        data : {
-          "link" :tab.url,
-        },
-        success: function(result){
-            var type = result.type;
-            var name = result.name;
-              $('#typeSpan').load("dialog.html",function(){
-                $('#typeSpan').text(type);
+        console.log(result);
+            if(result.success == 1){
+              var type = result.type;
+              var name = result.name;
+              chrome.storage.sync.set({'success' : 1,'type':type,'name':name,'link':tab.url},function(){
+                  console.log("SAVED!");
               });
-              $('#userNameSpan').load("dialog.html",function(){
-                $('#userNameSpan').text(name);
-              });
-
-        },error: function (error) {
-                  console.log("hatali");
+              if(result.type == "Human"){
+                  chrome.browserAction.setIcon({path:"icongreen.png"});
+              }else if(result.type== "Bot"){
+                  chrome.browserAction.setIcon({path:"iconred.png"});
+              }else{
+                  chrome.browserAction.setIcon({path:"icon.png"});
               }
+            }else{
+              chrome.storage.sync.set({'success' : result.success},function(){
+                  console.log("SAVED BUT RESULT FAILED");
+              });
+            }
+        },error: function (error) {
+                console.log("hatali");
+          }
       });
     }else{
         chrome.browserAction.setIcon({path:"icon.png"});
--- chromeapp/dialogg.js
+++ chromeapp/dialogg.js
@@ -1,73 +1,59 @@
-var type = "Human";
-var userName = "TEST";
 $(document).ready(function(){
     loaderVisibility(false);
-    var query = { active: true, currentWindow: true };
-    function callback(tabs) {
-      var currentTab = tabs[0];
-      if(currentTab.url.includes("twitter")){
-          $.ajax({
-            method :"POST",
-            dataType:"json",
-            url: "http://127.0.0.1:3547/user",
-            data : {
-              "link" :tab.url,
-            },
-            success: function(result){
-                //isteklerin degismesi lazim.
-                if(result == "Human"){
-                    chrome.browserAction.setIcon({path:"icongreen.png"});
-                        loaderVisibility(false);
-                }else if(result == "Bot"){
-                    chrome.browserAction.setIcon({path:"iconred.png"});
-                        loaderVisibility(false);
-                }else{
-                    chrome.browserAction.setIcon({path:"icon.png"});
-                    loaderVisibility(true);
-                }
-            }
-          });
+    chrome.storage.sync.get(['success', 'type','name','link'], function(items) {
+        if(items.success==1){
+          $("#userNameSpan").text(items.name);
+          $("#typeSpan").text(items.type);
+          $("#currentLink").val(items.link);
+        }else if (items.success == -1){
+          // sistem guncelleniyor yazmak gerek!
+        }else if (items.success == 0){
+          //hata olustugunu yazmak gerek!
         }
-    }
-    chrome.tabs.query(query, callback);
-});
-$("#humanBtn").click(function(e){
-    e.preventDefault();
-$.ajax({
-  method :"GET",
-  dataType:"json",
-  url: "http://127.0.0.1:3547/feedback",
-  data : {
-    "link" :tab.url,
-    "type" :'Human',
-  },
-  success: function(result){
-       thankMessage();
-  }
-});
-});
-$("#botBtn").click(function(e){
-    e.preventDefault();
-    $.ajax({
-      method :"GET",
-      url: "http://127.0.0.1:3547/feedback",
-      data : {
-        "link" :tab.url,
-        "type" :'Bot',
-      },
-      success: function(result){
-           thankMessage();
-      }
     });
-
+    $("#botBtn").click(function(e){
+        e.preventDefault();
+        loaderVisibility(true);
+        console.log("Bot clicked");
+        $.ajax({
+          method :"GET",
+          url: "http://127.0.0.1:3547/feedback",
+          data : {
+            "link" : $("#currentLink").val(),
+            "type" :'-1'
+          },
+          success: function(result){
+               thankMessage();
+          }
+        });
+    });
+    $("#humanBtn").click(function(e){
+        e.preventDefault();
+        loaderVisibility(true);
+        console.log("Human clicked");
+        $.ajax({
+          method :"GET",
+          url: "http://127.0.0.1:3547/feedback",
+          data : {
+            "link" : $("#currentLink").val(),
+            "type" :'1'
+          },
+          success: function(result){
+               console.log(result);
+               thankMessage();
+          }
+        });
+    });
 });
+
 function showDefaultPanel(){
   loaderVisibility(false);
 }
 function thankMessage(){
   loaderVisibility(false);
-  clearContext();
+  $("#context").hide();
   $("#context").html("<p style='color=#3b4047; font-size:larger;'><b>Thank you</b> for your feedback!</p>");
+  $("#context").show(1000);
 }
 function loaderVisibility(show){
   if(show){
